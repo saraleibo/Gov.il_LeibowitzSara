7707aff9d53a6d50bcd634597c1597a313459e0b
saraleibo <58260136+saraleibo@users.noreply.github.com>
2020-11-18 21:35:15 +0200

    Add files via upload

---
+++ GetApi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+
+namespace Gov_il
+{
+    public class GetApi
+    {
+        public void GetLanguageTitle()
+        {
+            string language = "/en";
+            string chackReg = "^[\u0000-\u007F]+$";
+;
+
+            RestClient restClient = new RestClient("https://www.gov.il");
+            RestRequest restRequest = new RestRequest(language, Method.GET);
+            restRequest.AddHeader("Accept", "application/json");
+            restRequest.RequestFormat = DataFormat.Json;
+
+            IRestResponse response = restClient.Execute(restRequest);
+            var content = response.Content;
+            string title = Regex.Match(content, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>",
+            RegexOptions.IgnoreCase).Groups["Title"].Value;
+            var isMatch = Regex.IsMatch(title, chackReg);
+            Assert.AreEqual(true, isMatch);
+        }
+    }
+}
+
---
+++ TestCase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gov_il
+{
+    [TestClass]
+    public class TestCase
+    {
+        [TestMethod]
+        public void CheckLanguageTitle()
+        {
+            GetApi getApi = new GetApi();
+            getApi.GetLanguageTitle();
+        }
+    }
+}
